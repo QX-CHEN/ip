29b63f31f8477290b6622cdeddfe3cbb54d6483b
QX-CHEN <c.qixing@yahoo.com.sg>
2020-08-23 11:47:43 +0800

    add addTask, list; disable echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    // Assume no more than 100 tasks
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         greet();
@@ -18,8 +22,17 @@ public class Duke {
         printHorizontalLine();
     }
 
+    public static void list() {
+        printHorizontalLine();
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+        }
+        printHorizontalLine();
+    }
+
     public static void greet() {
-        printMessage("\t Hello! I'm Duke\n\t What can I do for you?");
+        printMessage("\t Hello! I'm Duke" + System.lineSeparator() +
+                "\t What can I do for you?");
     }
 
     public static void bye() {
@@ -30,13 +43,21 @@ public class Duke {
         printMessage("\t " + str);
     }
 
+    public static void addTask(String task) {
+        tasks[taskCount++] = task;
+        printMessage("\t added: " + task);
+    }
+
     public static boolean executeCommand(String command) {
         boolean loop = true;
         if (command.equals("bye")) {
             bye();
             loop = false;
+        } else if (command.equals("list")) {
+            list();
         } else {
-            echo(command);
+//            echo(command);
+            addTask(command);
         }
         return loop;
     }
